cba01dd7f7ef04f8d6266b7bff5591be3933a5d1
kjx63 <ueyama.kenji.63@gmail.com>
2020-09-09 09:53:44 +0900

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Welcome to the ContactKeeper API...' });
+});
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// Production: PORT vs Development:5000
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
