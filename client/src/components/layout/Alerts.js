7669f5ad02d41574667d696fc91431de52e389fe
kjx63 <ueyama.kenji.63@gmail.com>
2020-09-11 09:50:40 +0900

    Alert Context, State & Build Alerts Components

---
+++ client/src/components/layout/Alerts.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext.js';
+
+const Alerts = () => {
+  const alertContext = useContext(AlertContext);
+  return alertContext?.alerts.map((alert) => (
+    <div key={alert.id} className={`alert alert-${alert.type}`}>
+      <i className='fas fa-info-circle' /> {alert.msg}
+    </div>
+  ));
+};
+
+export default Alerts;
